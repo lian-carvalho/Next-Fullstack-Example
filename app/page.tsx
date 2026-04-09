9bdb2202cfefa4b0ad381f4bd957b2c02aa85fa4
Lian Carvalho <liancarvalhodacosta@gmail.com>
2026-04-09 16:35:52 -0300

    Update Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,15 @@
-import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
+      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-center py-32 px-16 bg-white dark:bg-black sm:items-start">
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
+            Next Full Stack example with Prisma
           </h1>
         </div>
+        <br />
         <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
           <Link
             className="flex w-full items-center justify-center gap-2 rounded-full bg-foreground p-3 text-background"
